e2f5e9ff1d89277c8721c27cbf0c0f70e3aee653
Vladimir <v-for-vendeta@hotmail.rs>
2016-03-27 21:24:16 +0200

    Added start script

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 var middleware = require("./middleware.js")
 
@@ -13,6 +13,6 @@ app.get('/',middleware.requireAuthentication, function (req, res) {
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(port, function () {
+app.listen(PORT, function () {
     console.log("Server sterted!");
 });
